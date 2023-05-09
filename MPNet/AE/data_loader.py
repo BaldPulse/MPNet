0783241db84180033abf60b0d37a6543f623b7e8
Zhao Tang <tangzhao1999@hotmail.com>
2023-05-08 21:43:19 -0700

    trained AE

--- MPNet/AE/data_loader.py
+++ MPNet/AE/data_loader.py
@@ -3,10 +3,10 @@ import torch.utils.data as data
 import os
 import pickle
 import numpy as np
-import nltk
 from PIL import Image
 import os.path
 import random
+import open3d as o3d
 
 
 def load_dataset(N=30000,NP=1800):
@@ -15,6 +15,20 @@ def load_dataset(N=30000,NP=1800):
 	for i in range(0,N):
 		temp=np.fromfile('../dataset/obs_cloud/obc'+str(i)+'.dat')
 		temp=temp.reshape(int(len(temp)/2),2)
+		obstacles[i]=temp.flatten()
+
+	
+	return 	obstacles
+
+def load_dataset_JM(N=1000):
+
+	obstacles=np.zeros((N,6000),dtype=np.float32)
+	for i in range(0,N):
+		temp=o3d.io.read_point_cloud('../trajectory_data/obs_cloud/map_'+str(i+1)+'.ply')
+		temp=np.asarray(temp.points)
+		# downsample to 2000 points, choose randomly
+		temp_ind = np.random.choice(temp.shape[0], 2000, replace=False)
+		temp = temp[temp_ind]
 		obstacles[i]=temp.flatten()
 
 	
--- visualizer.py
+++ visualizer.py
@@ -16,24 +16,24 @@ def main(args):
 
 
 
-    # visualize path
-    path = np.loadtxt(args.path_file)
-    print(path)
-    path = path.reshape(-1, 2)
-    path_x = []
-    path_y = []
-    for i in range(len(path)):
-        path_x.append(path[i][0])
-        path_y.append(path[i][1])
+    # # visualize path
+    # path = np.loadtxt(args.path_file)
+    # print(path)
+    # path = path.reshape(-1, 2)
+    # path_x = []
+    # path_y = []
+    # for i in range(len(path)):
+    #     path_x.append(path[i][0])
+    #     path_y.append(path[i][1])
 
-    plt.plot(path_x, path_y, c='r', marker='o')
+    # plt.plot(path_x, path_y, c='r', marker='o')
 
     plt.show()
 
 
 parser = argparse.ArgumentParser()
 # for training
-parser.add_argument('--obs_file', type=str, default='./data/obs_cloud/obc0.dat',help='obstacle point cloud file')
+parser.add_argument('--obs_file', type=str, default='./dataset/obs_cloud/obc0.dat',help='obstacle point cloud file')
 parser.add_argument('--path_file', type=str, default='./results/env_0/path_0.txt',help='path file')
 args = parser.parse_args()
 print(args)
